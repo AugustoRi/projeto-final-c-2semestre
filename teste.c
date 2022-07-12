5e9a944f102014d284c3706b387670b68dd09aea
edumfelix <dudumacedofelix613@gmail.com>
2022-07-12 14:10:06 -0300

    Numeros Binarios

--- teste.c
+++ teste.c
@@ -4,101 +4,121 @@
 #include <math.h>
 
 #define TAM 3
-#define MAX 3
+
 // PROTÓTIPOS
-void preencherMatriz(int **p);
-void imprimirMatriz(int **p);
-void verificarMatriz(int **p);
-int converterBinario(int *bin, int **p);
+void preencherMatriz(int *p);
+void imprimirMatriz(int *p);
+void verificarMatriz(int *p);
+int converterBinario(int *bin, int *p);
+
+struct Matriz {
+  int col;
+  int lin;
+  int *p_matriz;
+};
 
 int main() {
-  int **p_matriz = NULL;
+  struct Matriz matriz;
   int *p_bin = NULL;
+  matriz.col = TAM;
+  matriz.lin = TAM;
 
   // === Alocando a matriz dinamicamente
   p_bin = malloc(sizeof(int) * 8);
-  p_matriz = malloc(sizeof(int) * TAM);
-  for (int i=0; i < TAM; i++){
-   p_matriz[i] = malloc (TAM * sizeof (int));
-  }
+  matriz.p_matriz = malloc(matriz.col * matriz.lin * sizeof(int));
   
-  preencherMatriz(p_matriz);
-  imprimirMatriz(p_matriz);
+  preencherMatriz(matriz.p_matriz);
+  imprimirMatriz(matriz.p_matriz);
   printf("\n\n");
 
-  verificarMatriz(p_matriz);
-  imprimirMatriz(p_matriz);
-  printf("\n%d", converterBinario(p_bin, p_matriz));
+  verificarMatriz(matriz.p_matriz);
+  imprimirMatriz(matriz.p_matriz);
+  printf("Binario convertido para decimal: %d", converterBinario(p_bin, matriz.p_matriz));
 
 }
 
-void preencherMatriz(int **p) {
+void preencherMatriz(int *p) {
   srand(time(NULL));
-  for (int i=0; i < TAM; i++){
-    for (int j=0; j < TAM; j++){
-        p[i][j] = rand() % 100;
-    }
+  for (int i=0; i < TAM * TAM; i++){
+        *(p + i) = rand() % 100;
   }
 }
 
-void verificarMatriz(int **p) {
-  for (int j = 0; j < TAM; j++) {
-    for (int k = 0; k < TAM; k++) {
-      if (p[j][k] < p[1][1]) {
-        p[j][k] = 0;
-      } else if (p[j][k] > p[1][1]) {
-        p[j][k] = 1;
+void verificarMatriz(int *p) {
+  for (int j = 0; j < TAM * TAM; j++) {
+
+      if (*(p + j) < *(p + 4)) {
+        *(p + j) = 0;
+      } else if (*(p + j) > *(p + 4)) {
+        *(p + j) = 1;
       } else {
-        p[j][k] = p[j][k];
-      }
+        *(p + j) = *(p + j);
     }
   }
 }
 
-void imprimirMatriz(int **p) {
-  for (int j = 0; j < TAM; j++) {
-    printf("\n");
-    for (int k = 0; k < TAM; k++) {
-      printf("%3d |", p[j][k]);
+void imprimirMatriz(int *p) {
+  for (int j = 0; j < TAM * TAM; j++) {
+    if (j % 3 == 0){
+      printf("\n");
     }
+      printf("%3d |", *(p + j));
   }
 }
 
-int converterBinario(int *bin, int **p) {
+int converterBinario(int *bin, int *p) {
   int i = 0, decimal = 0, m = 1;
-  int v[8];
   //FUNCAO PARA PEGAR APENAS O BINARIO
-  for (int j = 0; j < TAM; j++) {
-    for (int k = 0; k < TAM; k++) {
-      if (p[j][k] != p[1][1]){
-      *(bin + i) = p[j][k];
+
+  for (int j = 0; j < TAM * TAM; j++) {
+      if (*(p + j) != *(p + 4)){
+      *(bin + i) = *(p + j);
       i++;
       }
-    }
-  } 
-  //ORDENAÇAO DA MATRIZ MANUALMENTE
-  v[0] = bin[3];
-  v[1] = bin[5];
-  v[2] = bin[6];
-  v[3] = bin[7];
-  v[4] = bin[4];
-  v[5] = bin[2];
-  v[6] = bin[1];
-  v[7] = bin[0];
+    } 
+  //ORDENAÇAO DA MATRIZ
 
-printf("\n\n");
-  for(int r = 0; r < 8; r++) printf("%3d | ", v[r]);
+	// //primeiro for
+  int a = 0;
+  printf("\n\n");
+	for (int j = 3; j < TAM * TAM; j += 3 ) {
+		*(bin + a) = *(p + j);
+    printf("%d ", *(bin + a));
+		a++;
+	}
 
+	//segundo for
+	for (int k = TAM * TAM - 2; k < TAM * TAM; k++) {
+		*(bin + a) = *(p + k);
+    printf("%d ", *(bin + a));
+		a++;
+	}
 
+	//terceiro for 
+	for (int l = TAM * TAM - 3; l > 0; l = l - 3) {
+		*(bin + a) = *(p + l);
+    printf("%d ", *(bin + a));
+		a++;
+	}
+
+	//quarto for
+	for (int h = 1; h >= 0; h -= 1) {
+		*(bin + a) = *(p + h);
+    printf("%d ", *(bin + a));
+		a++;
+	}
+
+// ================
+   printf("\n\n");
+  // for(int r = 0; r < 8; r++) printf("%3d | ", bin[r]);
 
 
   //CONVERTER BINARIO PARA DECIMAL
   for(int a = 7; a > -1; a--){
-    if( v[a] == 1 ){
-      decimal += v[a] * m;
+    if( bin[a] == 1 ){
+      decimal += bin[a] * m;
     }
     m *= 2;
   }
   return decimal;
 }
-
