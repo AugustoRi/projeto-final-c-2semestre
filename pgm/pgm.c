d2a6cdc189cfd4a17e9fc083e5c387f8709fe7c6
augustoribeirodev <franciscoaugustodev@gmail.com>
2022-07-12 09:38:21 -0300

    feat estrutura exemplo leitura de imagens

--- pgm/pgm.c
+++ pgm/pgm.c
@@ -1,10 +1,20 @@
 #include "functions.h"
+#include <dirent.h>
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[]) {
+	DIR *d;
+	struct dirent *dir;
+	d = opendir("../datasets/oncotex_pgm");
 	struct pgm img;
 
-	if (argc!=3){
+	if (d) {
+		while ((dir = readdir(d)) != NULL) {
+			printf("%s\n", dir->d_name);
+		}
+		closedir(d);
+	}
+
+	if (argc!=3) {
 		printf("Formato: \n\t %s <imagemEntrada.pgm> <imagemSaida.pgm>\n",argv[0]);
 		exit(1);
 	}
